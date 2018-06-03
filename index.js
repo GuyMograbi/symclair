6b1cd82f172e5839cdc87a64a1f6a744a3bc5350
Guy Mograbi <guy.mograbi@gmail.com>
2018-06-03 16:39:20 -0700

    finish implementation

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const fs = require('fs-extra');
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+console.log(argv._);
+
+argv._.map(f=>({
+  filepath: path.resolve(f),
+  realPath: fs.realpathSync(f)
+})).filer(item=>item.realPath !== item.filepath).forEach(item=>{
+  fs.copySync(item.realPath, item.filepath);
+});
